fe9883e30358c8d003a306b10f0cc7d0a2105956
Depinoy Thomas <tdepinoy.externe@pagesjaunes.fr>
2017-06-16 17:17:47 +0200

    initialize repository

---
+++ app/src/main/java/tdepinoy/kotlinpotier/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package tdepinoy.kotlinpotier.home
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import tdepinoy.kotlinpotier.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
